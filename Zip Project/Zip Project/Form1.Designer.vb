c2ee63517a7eaa26ff5c37c8e5500a08332ecae7
Zachary Murphy <ztmurphy21@gmail.com>
2017-03-07 19:57:24 -0500

    assignment 5

---
+++ Zip Project/Zip Project/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstCities = New System.Windows.Forms.ListBox()
+        Me.btnFindZips = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lstZips = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(28, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(205, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Please select a city:"
+        '
+        'lstCities
+        '
+        Me.lstCities.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstCities.FormattingEnabled = True
+        Me.lstCities.ItemHeight = 26
+        Me.lstCities.Location = New System.Drawing.Point(32, 51)
+        Me.lstCities.Name = "lstCities"
+        Me.lstCities.Size = New System.Drawing.Size(150, 134)
+        Me.lstCities.TabIndex = 1
+        '
+        'btnFindZips
+        '
+        Me.btnFindZips.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFindZips.Location = New System.Drawing.Point(550, 73)
+        Me.btnFindZips.Name = "btnFindZips"
+        Me.btnFindZips.Size = New System.Drawing.Size(186, 37)
+        Me.btnFindZips.TabIndex = 3
+        Me.btnFindZips.Text = "Show Zipcodes"
+        Me.btnFindZips.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(550, 176)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(186, 36)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(550, 120)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(186, 37)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lstZips
+        '
+        Me.lstZips.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstZips.FormattingEnabled = True
+        Me.lstZips.ItemHeight = 26
+        Me.lstZips.Location = New System.Drawing.Point(289, 51)
+        Me.lstZips.Name = "lstZips"
+        Me.lstZips.Size = New System.Drawing.Size(150, 134)
+        Me.lstZips.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(284, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(277, 26)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Zipcode(s) for selected city:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(736, 292)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lstZips)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnFindZips)
+        Me.Controls.Add(Me.lstCities)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Zip Code Finder"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lstCities As ListBox
+    Friend WithEvents btnFindZips As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lstZips As ListBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Zip Project/Zip Project/Form1.vb
@@ -0,0 +1,59 @@
+﻿'zip program
+'Purpose: Outputs the zipcode of a city, based off select from box list.
+'Programmer: Zachary Murphy on 3-7-2017
+
+Public Class Form1
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click, lstCities.SelectedIndexChanged
+        lstZips.Items.Clear()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'closes application
+        Me.Close()
+    End Sub
+
+    Private Sub btnFindZips_Click(sender As Object, e As EventArgs) Handles btnFindZips.Click
+        Dim intZip As Integer
+
+        'determines the zip code based off selected city.
+        If (lstCities.SelectedItem = "Baxley") Then
+            For intZip = 31513 To 31515 Step 2
+                lstZips.Items.Add(intZip.ToString)
+            Next intZip
+
+        ElseIf (lstCities.SelectedItem = "Newton") Then
+            For intZip = 39870 To 39870
+                lstZips.Items.Add(intZip.ToString)
+            Next intZip
+
+        ElseIf (lstCities.SelectedItem = "Adairsville") Then
+            For intZip = 30103 To 30103
+                lstZips.Items.Add(intZip.ToString)
+            Next intZip
+
+        ElseIf (lstCities.SelectedItem = "Statesboro") Then
+            For intZip = 30458 To 30461 Step 1
+                lstZips.Items.Add(intZip.ToString)
+            Next intZip
+
+        ElseIf (lstCities.SelectedItem = "Canton") Then
+            For intZip = 30114 To 30115 Step 1
+                lstZips.Items.Add(intZip.ToString)
+            Next intZip
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'fill list box with cities
+        lstCities.Items.Add("Baxley")
+        lstCities.Items.Add("Newton")
+        lstCities.Items.Add("Adairsville")
+        lstCities.Items.Add("Statesboro")
+        lstCities.Items.Add("Canton")
+    End Sub
+
+
+
+End Class
